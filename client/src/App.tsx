33f1eedf5bfb2ac38a4a324e5e9b0fdceae79d8b
David Enikuomehin <enikuauth@gmail.com>
2023-11-22 14:51:59 +0100

    chore: import required assets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,5 @@
 export default function App() {
-    return <div className='text-3xl text-red-500'>Hello World!</div>;
+    return (
+        <div className='text-3xl text-red-500 font-mansalva'>Hello World!</div>
+    );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                manrope: ['Manrope', 'sans-serif'],
+                mansalva: ['Mansalva', 'sans-serif'],
+            },
+            colors: {
+                primary: {
+                    DEFAULT: '#7E41F5',
+                    light: '#CCB2FF',
+                },
+                mono: {
+                    light: '#878787',
+                    dark: '#121212',
+                },
+                gray: {
+                    DEFAULT: '#E3E3E3',
+                    light: '#F4F4F4',
+                },
+            },
+        },
     },
     plugins: [],
 };
