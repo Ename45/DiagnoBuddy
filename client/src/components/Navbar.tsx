3a6d4c221103da848c1ebf3b3ffafef82d09b82b
David Enikuomehin <enikuauth@gmail.com>
2023-11-23 23:27:23 +0100

    feat(navbar): relfect changes in design

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import chevronLeftIcon from '../assets/svg/chevron-left.svg';
 import closeIcon from '../assets/svg/close.svg';
 
 import avatar from '../assets/images/ai.png';
-import modalImg from '../assets/images/modal.png';
+import modalImg from '../assets/images/folder.png';
 
 export default function Navbar() {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -32,94 +32,93 @@ export default function Navbar() {
 
     return (
         <>
-            <nav className='flex items-center justify-between py-6 px-4 bg-[#E8E3F3] lg:px-[150px] lg:py-6 lg:bg-[#E8E3F3]/50 font-manrope text-mono-dark '>
-                <div className='flex gap-4 items-center'>
-                    {/* Icon that displays only on Mobile View */}
-                    <img
-                        loading='lazy'
-                        src={chevronLeftIcon}
-                        alt=''
-                        className='lg:hidden'
-                    />
+            <nav className=' bg-[#E8E3F3] font-manrope text-mono-dark sticky top-0 z-10'>
+                <div className='flex items-center justify-between py-6 px-4 lg:px-[150px] lg:py-6 max-width lg:bg-white/50'>
+                    <div className='flex gap-4 items-center'>
+                        {/* Icon that displays only on Mobile View */}
+                        <img
+                            loading='lazy'
+                            src={chevronLeftIcon}
+                            alt=''
+                            className='lg:hidden'
+                        />
 
-                    {/* Avatar that displays only on Desktop View */}
-                    <img
-                        loading='lazy'
-                        src={avatar}
-                        alt=''
-                        className='hidden lg:block w-[60px] h-[50px]'
-                    />
+                        {/* Avatar that displays only on Desktop View */}
+                        <img
+                            loading='lazy'
+                            src={avatar}
+                            alt=''
+                            className='hidden lg:block w-[60px] h-[50px]'
+                        />
 
-                    <div className='space-y-2'>
-                        <h3 className='font-bold text-xl'>Diagnobuddy</h3>
+                        <div className='space-y-2'>
+                            <h3 className='font-bold text-xl'>Diagnobuddy</h3>
 
-                        <p className='hidden lg:block'>online</p>
+                            <p className='hidden lg:block'>online</p>
+                        </div>
                     </div>
+
+                    <button
+                        onClick={toggleModal}
+                        className='lg:px-4 lg:py-2 lg:bg-white lg:flex gap-4 items-center lg:rounded-full'
+                    >
+                        <img
+                            loading='lazy'
+                            src={uploadIcon}
+                            alt=''
+                            className='lg:hidden'
+                        />
+
+                        <img
+                            loading='lazy'
+                            src={uploadBlueIcon}
+                            alt=''
+                            className='hidden lg:block'
+                        />
+
+                        <p className='hidden lg:block font-bold'>Save Chat</p>
+
+                        <span className='sr-only'>
+                            Click to save a version of this conversation to your
+                            email address
+                        </span>
+                    </button>
                 </div>
-
-                <button
-                    onClick={toggleModal}
-                    className='lg:px-4 lg:py-2 lg:bg-white lg:flex gap-4 items-center lg:rounded-full'
-                >
-                    <img
-                        loading='lazy'
-                        src={uploadIcon}
-                        alt=''
-                        className='lg:hidden'
-                    />
-
-                    <img
-                        loading='lazy'
-                        src={uploadBlueIcon}
-                        alt=''
-                        className='hidden lg:block'
-                    />
-
-                    <p className='hidden lg:block font-bold'>Save Chat</p>
-
-                    <span className='sr-only'>
-                        Click to save a version of this conversation to your
-                        email address
-                    </span>
-                </button>
             </nav>
 
             <AnimatePresence>
-                {
-                    // Modal Overlay
-                    isModalOpen && (
+                {isModalOpen && (
+                    <div className='grid place-items-center fixed inset-0 z-50'>
+                        {/* // Modal Overlay */}
+
                         <motion.div
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 0.3 }}
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.2, ease: 'easeOut' }}
                             className='fixed top-0 left-0 w-full h-full bg-black z-10'
-                        ></motion.div>
-                    )
-                }
+                        />
 
-                {
-                    // Popup Modal
-                    isModalOpen && (
+                        {/* Modal */}
                         <motion.div
                             initial={
                                 windowWidth >= 1010
-                                    ? { opacity: 0 }
+                                    ? { opacity: 0, scale: 0.6 }
                                     : { opacity: 0.8, y: 500 }
                             }
                             animate={
                                 windowWidth >= 1010
-                                    ? { opacity: 1 }
+                                    ? { opacity: 1, scale: 1 }
                                     : { opacity: 1, y: 0 }
                             }
                             exit={
                                 windowWidth >= 1010
-                                    ? { opacity: 0 }
-                                    : { opacity: 0.8, y: 500 }
+                                    ? { opacity: 0, scale: 0.6 }
+                                    : { opacity: 1, y: 500 }
                             }
                             transition={{ duration: 0.3, ease: 'easeOut' }}
                             className='font-manrope fixed bottom-0 left-0 right-0 z-50 h-fit rounded-t-[60px] bg-white px-9
-                        lg:w-[500px] lg:h-[500px] lg:rounded-[20px] lg:m-auto lg:-top-1/2 lg:translate-y-1/2'
+                        lg:w-[500px] lg:h-[500px] lg:rounded-[20px] lg:static'
                         >
                             <button
                                 onClick={toggleModal}
@@ -136,15 +135,17 @@ export default function Navbar() {
                                 loading='lazy'
                                 src={modalImg}
                                 alt=''
-                                className='mx-auto -mb-14 w-[230px] h-[321px]'
+                                width={120}
+                                height={98}
+                                className='mx-auto mt-[95px]'
                             />
 
-                            <h3 className='max-w-[360px] text-xl leading-8 text-center mx-auto'>
+                            <h3 className='max-w-[255px] text-lg font-medium leading-8 text-center mx-auto my-8 lg:text-xl lg:max-w-[374px] lg:mt-7'>
                                 Send a saved version of this conversation to
                                 your email address.
                             </h3>
 
-                            <div className='max-w-[250px] font-bold space-y-5 mx-auto mb-12 mt-8'>
+                            <div className='max-w-[210px] font-bold space-y-5 mx-auto mb-20 lg:text-xl lg:max-w-[250px]'>
                                 <button
                                     onClick={toggleModal}
                                     className='py-4 bg-primary text-white w-full rounded-full'
@@ -160,8 +161,8 @@ export default function Navbar() {
                                 </button>
                             </div>
                         </motion.div>
-                    )
-                }
+                    </div>
+                )}
             </AnimatePresence>
         </>
     );
