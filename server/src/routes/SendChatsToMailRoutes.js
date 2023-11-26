af03b89a65653ef28fb15b9de86a9197e1725fdd
David Enikuomehin <enikuauth@gmail.com>
2023-11-26 18:32:41 +0100

    chore: delete folder

--- server/src/routes/SendChatsToMailRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const SendChatsToMailController = require("../controllers/SendChatsToMailController")
-
-
-router.post("/", SendChatsToMailController.sendChat)
-
-
-module.exports = router
